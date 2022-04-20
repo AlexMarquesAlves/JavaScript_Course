82539334c0b7afb6ea11d0dea35277a67a7b6803
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-04-20 12:48:22 -0300

    Comentários de código em JS

---
+++ --1.basico👇👇/aula002/comentarios.js
@@ -0,0 +1,15 @@
+// E escrever comentário
+console.log(`Olá Mundo`); // Aqui tem outro comentário
+console.log(`linha 1`);
+// comentário
+
+/**
+ * Um Titulo 
+ * 
+ * Lorem ipsum
+ * */
+
+console.log(`linha 2`);
+console.log(`linha 3`);
+console.log(`linha 4`);
+// vai ser ignorado
