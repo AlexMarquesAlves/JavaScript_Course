ff17bb1465c25ac0a14e3e63fde305d5734b83ac
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-04-23 15:04:59 -0300

    Regras para o uso de constantes em JS

---
+++ --1.basico👇👇/aula005/index.js
@@ -0,0 +1,13 @@
+/**
+ * Regras para uso de Constantes
+ *
+ * Não podemos usar palavras reservadas para criá-las.
+ * Precisam possuir nomes significativos.
+ * Não pode iniciar-se com um número.
+ * Não pode conter espaços ou traços.
+ * Utiliza-se camelCase.
+ * Case-sensitive.
+ * Não pode-se modificar o valor de uma constante
+ * NÃO UTILIZE VAR, UTILIZE COST.
+ */
+
