290895e55c83122a58c895968eacfceed4a02505
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-04-23 15:57:55 -0300

    Tipos primitivos -> boolean

--- --1.basico👇👇/aula010/index.js
+++ --1.basico👇👇/aula010/index.js
@@ -6,4 +6,6 @@ const num1 = 10; // Number
 const num2 = 10.52; // Number
 
 let nomeAluno; // undefined -> não aponta para nenhum local na memoria
-let sobrenomeAluno = null; // Null -> não aponta para nenhum local na memoria
+const sobrenomeAluno = null; // Null -> não aponta para nenhum local na memoria
+const booleanOn = true; // Boolean true, false
+const booleanOff = false; // Boolean true, false
