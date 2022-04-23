ede52211a25694a264cd3806f8aea61c55b1dc34
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-04-23 15:35:09 -0300

    Exibindoo Resultado no console usando template strings

--- --1.basico👇👇/aula008/index.js
+++ --1.basico👇👇/aula008/index.js
@@ -14,3 +14,8 @@ let anoNascimento;
 indiceMassaCorporal = peso / (alturaEmM * alturaEmM);
 anoNascimento = 2019 - idade;
 
+// template strings
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`);
+console.log(`tem ${alturaEmM} de altura e seu IMC é de ${indiceMassaCorporal}`);
+console.log(`${nome} nasceu em ${anoNascimento}.`);
