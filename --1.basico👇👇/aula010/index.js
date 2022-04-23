6ce4bb0f666d6f479744cdc0bffc2c6aa855c823
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-04-23 15:54:19 -0300

    Tipos Primitivos - > null

--- --1.basico👇👇/aula010/index.js
+++ --1.basico👇👇/aula010/index.js
@@ -6,3 +6,4 @@ const num1 = 10; // Number
 const num2 = 10.52; // Number
 
 let nomeAluno; // undefined -> não aponta para nenhum local na memoria
+let sobrenomeAluno = null; // Null -> não aponta para nenhum local na memoria
