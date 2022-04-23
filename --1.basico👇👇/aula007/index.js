15d27f08e7d3fdcd4117cc69d5f241fc1aa0378f
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-04-23 15:22:19 -0300

    Duplicando valor

--- --1.basico👇👇/aula007/index.js
+++ --1.basico👇👇/aula007/index.js
@@ -18,3 +18,5 @@ const primeiroNumero = 5;
 const segundoNumero = 10;
 const resultado = primeiroNumero * segundoNumero;
 console.log(resultado);
+const resultadoDuplicado = resultado * 2;
+console.log(resultadoDuplicado);
