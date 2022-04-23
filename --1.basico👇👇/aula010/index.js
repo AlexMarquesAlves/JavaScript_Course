7864205eb7720db0fa555b1d4445517a97b9316a
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-04-23 16:01:53 -0300

    Exibindo os tipos no console

--- --1.basico👇👇/aula010/index.js
+++ --1.basico👇👇/aula010/index.js
@@ -9,3 +9,10 @@ let nomeAluno; // undefined -> não aponta para nenhum local na memoria
 const sobrenomeAluno = null; // Null -> não aponta para nenhum local na memoria
 const booleanOn = true; // Boolean true, false
 const booleanOff = false; // Boolean true, false
+
+console.log(typeof nome0,nome0);
+console.log(typeof num1,num1);
+console.log(typeof nomeAluno,nomeAluno);
+console.log(typeof sobrenomeAluno,sobrenomeAluno);
+console.log(typeof booleanOn,booleanOn);
+console.log(typeof booleanOff,booleanOff);
