53089206912915e15a2435bf0b6286a3073d5124
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-10 20:44:00 -0300

    Exemplo de Decremento

--- --1.basico👇👇/aula011/index.js
+++ --1.basico👇👇/aula011/index.js
@@ -7,6 +7,7 @@
 const num1 = 5;
 const num2 = 2;
 const num3 = 10;
+console.log(`Exemplo 1`);
 console.log((num1 + num2) * num3);
 
 /**  Precedência dos operadores aritméticos
@@ -17,9 +18,15 @@ console.log((num1 + num2) * num3);
  * + - Adição y Subtração
  */
 
-let contador = 1;
-contador++; // 2
-contador++; // 3
-contador++; // 4
-contador++; // 5
+let contador = 10;
+contador++; // 11
+++contador; // 12
+contador++; // 13
+++contador; // 14
+console.log(`Exemplo incremento ++`);
 console.log(contador);
+console.log(`Exemplo decremento --`);
+console.log(--contador); // 13
+console.log(--contador); // 12
+console.log(--contador); // 11
+console.log(--contador); // 10
