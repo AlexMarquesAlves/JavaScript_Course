5c010763e7566b91aa78cc25613e6a4d9a385280
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-10 20:25:37 -0300

    Contador

--- --1.basico👇👇/aula011/index.js
+++ --1.basico👇👇/aula011/index.js
@@ -5,3 +5,10 @@
  * / % Divisão y Resto da divisão
  * + - Adição y Subtração
  */
+
+let contador = 1;
+contador++; // 2
+contador++; // 3
+contador++; // 4
+contador++; // 5
+console.log(contador);
