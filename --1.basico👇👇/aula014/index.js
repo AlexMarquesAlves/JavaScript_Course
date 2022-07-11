410f59ebcdd696b02c9cfc4a808e4be3b1bd33ac
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-11 12:50:18 -0300

    Mais sobre strings --> busca usando espressões regulares

--- --1.basico👇👇/aula014/index.js
+++ --1.basico👇👇/aula014/index.js
@@ -1,4 +1,4 @@
-let umaString = "Um texto";
+let umaString = "O rato roeu a roupa do rei de roma";
 
 console.log(umaString.concat(" em um lindo dia."));
 console.log(umaString + " em um lindo dia.");
@@ -9,3 +9,7 @@ console.log(umaString.indexOf("o", 3));
 
 console.log(umaString.lastIndexOf("o"));
 console.log(umaString.lastIndexOf("m", 3));
+
+console.log(umaString.match(/[a-z]/g));
+console.log(umaString.search(/x/));
+console.log(umaString.replace(/r/g, "p"));
