c557247a8b504a6312f6b5a443561a4ca1da56da
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-11 00:06:57 -0300

    Resolução moderna

--- --1.basico👇👇/aula013/index.js
+++ --1.basico👇👇/aula013/index.js
@@ -3,10 +3,13 @@ let varA = "A"; // B
 let varB = "B"; // C
 let varC = "C"; // A
 
-//Solução
-const temp = varA;
-varA = varB;
-varB = varC;
-varC = temp;
+// Solução
+// const temp = varA;
+// varA = varB;
+// varB = varC;
+// varC = temp;
+
+// Solução moderna
+[varA, varB, varC] = [varB, varC, varA];
 
 console.log(varA, varB, varC);
