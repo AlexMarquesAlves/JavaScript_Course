2ae8310639e10513cd25a1dbacba0312614e983d
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-11 12:41:41 -0300

    Mais sobre strings --> buscando pelo indice final

--- --1.basico👇👇/aula014/index.js
+++ --1.basico👇👇/aula014/index.js
@@ -6,3 +6,6 @@ console.log(`${umaString} em um lindo dia.`);
 
 console.log(umaString.indexOf("texto"));
 console.log(umaString.indexOf("o", 3));
+
+console.log(umaString.lastIndexOf("o"));
+console.log(umaString.lastIndexOf("m", 3));
