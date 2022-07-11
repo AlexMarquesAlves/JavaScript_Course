89cd7370b47b264c1e915a07ff2dcca97751c71e
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-10 23:18:06 -0300

    Resolução do exercicio

--- --1.basico👇👇/aula012/js/script.js
+++ --1.basico👇👇/aula012/js/script.js
@@ -1,3 +1,20 @@
 let name = prompt(`Olá! Qual vosso nome? `);
 alert(`Bem-Vindo! 😍 ${name}`);
 // confirm(`Realmente desejas Sair 😒? `);
+
+// Exercicio
+const num1 = Number(prompt(`Digite um número. `));
+const num2 = Number(prompt(`Digite outro número. `));
+let soma = num1 + num2;
+let diferenca = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+
+console.log(`  -->Soma`);
+console.log(`A Soma ${num1} + ${num2} = ${soma}`);
+console.log(`  -->Subtração`);
+console.log(`A Subtração ${num1} - ${num2} = ${diferenca}`);
+console.log(`  -->Multiplicação`);
+console.log(`A Multiplicação ${num1} * ${num2} = ${multiplicacao}`);
+console.log(`  -->Divisão`);
+console.log(`A Divisão ${num1} / ${num2} = ${divisao}`);
