537ebfc50fc8297ccb6104153547e9b4f04ea4cb
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-12 00:57:57 -0300

    Resolvendo a imprecisão com ParseFloat

--- --1.basico👇👇/aula015/index.js
+++ --1.basico👇👇/aula015/index.js
@@ -7,7 +7,7 @@ num1 += num2; //0.8
 num1 += num2; //0.9
 num1 += num2; //1.0
 
-num1 = num1.toFixed(2);
+num1 = parseFloat(num1.toFixed(2));
 
 console.log(num1);
 console.log(Number.isInteger(num1));
