adc2ae8fbcdbabe0dcdbfc738869f0655e3a344e
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-12 01:04:09 -0300

    Verificando se é inteiro

--- --1.basico👇👇/aula015/index.js
+++ --1.basico👇👇/aula015/index.js
@@ -4,6 +4,8 @@ let num1 = 0.7; // number
 let num2 = 0.1; // number
 
 num1 = (num1 * 100 + num2 * 100) / 100; //0.8
+num1 = (num1 * 100 + num2 * 100) / 100; //0.9
+num1 = (num1 * 100 + num2 * 100) / 100; //1.0
 
 console.log(num1);
 console.log(Number.isInteger(num1));
