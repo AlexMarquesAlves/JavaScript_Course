86240220f2ba721f467bb8613f4f1faa45dcf21e
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-12 00:49:42 -0300

    Verificando se algum valor is NaN

--- --1.basico👇👇/aula015/index.js
+++ --1.basico👇👇/aula015/index.js
@@ -5,4 +5,6 @@ let num2 = 2.5; // number
 // num1 = num1.toString();
 // console.log(num1.toString(2));
 // console.log(num1.toFixed(2));
-console.log(Number.isInteger(num1));
+// console.log(Number.isInteger(num1));
+let temp = num1 * "ola";
+console.log(Number.isNaN(temp));
