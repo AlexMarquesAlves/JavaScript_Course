d3b43d9e090b7d9f66995292d9dad7a37fcb2b7b
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-12 00:59:29 -0300

    Incrementando ++

--- --1.basico👇👇/aula015/index.js
+++ --1.basico👇👇/aula015/index.js
@@ -6,6 +6,11 @@ let num2 = 0.1; // number
 num1 += num2; //0.8
 num1 += num2; //0.9
 num1 += num2; //1.0
+num1 += num2; //1.1
+num1 += num2; //1.2
+num1 += num2; //1.3
+num1 += num2; //1.4
+num1 += num2; //1.5
 
 num1 = parseFloat(num1.toFixed(2));
 
