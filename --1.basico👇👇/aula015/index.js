07afb63aa8a921501897d6f4979a06f0c30fa235
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-12 00:43:42 -0300

    Exibindo valor binário com o metodo toString

--- --1.basico👇👇/aula015/index.js
+++ --1.basico👇👇/aula015/index.js
@@ -1,5 +1,6 @@
-let num1 = 10; // number
+let num1 = 1500; // number
 let num2 = 2.5; // number
 
 // console.log(num1.toString() + num2);
-num1 = num1.toString();
+// num1 = num1.toString();
+console.log(num1.toString(2));
