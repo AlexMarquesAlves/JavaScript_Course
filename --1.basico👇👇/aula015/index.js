9ffdc951e8093b80c24be6f365e2eee50bb30e21
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-12 00:45:14 -0300

    Arredondando valor decimal

--- --1.basico👇👇/aula015/index.js
+++ --1.basico👇👇/aula015/index.js
@@ -1,6 +1,7 @@
-let num1 = 1500; // number
+let num1 = 10.5798551255547; // number
 let num2 = 2.5; // number
 
 // console.log(num1.toString() + num2);
 // num1 = num1.toString();
-console.log(num1.toString(2));
+// console.log(num1.toString(2));
+console.log(num1.toFixed(2));
