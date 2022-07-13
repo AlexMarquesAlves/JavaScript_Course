c202455590254016ba4311c08860cca8ec8d6865
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-13 18:57:43 -0300

    const aleatorio= gerando numero aleatório entre 10 - 5

--- --1.basico👇👇/aula016/index.js
+++ --1.basico👇👇/aula016/index.js
@@ -6,4 +6,5 @@ let num1 = 9.54578;
 console.log(Math.max(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6));
 console.log(Math.min(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6));
 
-console.log(Math.random());
+const aleatorio = Math.random() * (10 - 5) + 5;
+console.log(aleatorio);
