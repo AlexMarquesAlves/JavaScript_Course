4b8c728a185e5f4f27ef46926d2791a348fa51fb
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-13 19:06:22 -0300

    Objeto Math --> possibilidade de dividir um numero p/ 0

--- --1.basico👇👇/aula016/index.js
+++ --1.basico👇👇/aula016/index.js
@@ -1,7 +1,7 @@
-let num1 = 9;
+// let num1 = 9;
 
-console.log(num1 ** (1 / 2));
-console.log(num1 ** 0.5);
+// console.log(num1 ** (1 / 2));
+// console.log(num1 ** 0.5);
 
 // let num2 = Math.floor(num1);
 // let num2 = Math.ceil(num1);
@@ -11,4 +11,6 @@ console.log(num1 ** 0.5);
 // console.log(Math.min(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6));
 
 // const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
-console.log(Math.pow(2, 10));
+// console.log(Math.pow(2, 10));
+
+console.log(100 / 0);
