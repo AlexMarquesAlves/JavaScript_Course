e756888bfb0a81c60ae152a9888ea22ab2eb01f9
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-14 00:51:30 -0300

    Array --> Adicionando no Começo do array

--- --1.basico👇👇/aula017/index.js
+++ --1.basico👇👇/aula017/index.js
@@ -1,15 +1,18 @@
 const alunos = ["Luiz", "Maria", "João"];
 
-alunos.push("Otávio");
-alunos.push("Miranda");
+alunos.unshift("Luíza"); // Adiciona no Começo
+alunos.unshift("Fábio");
 
 console.log(alunos);
 
-// alunos[alunos.length] = "Luiza";
+// alunos.push("Otávio"); // Adiciona no Fim
+// alunos.push("Miranda");
+
+// alunos[alunos.length] = "Luiza"; // Adiciona no Fim
 // alunos[alunos.length] = "Fabio";
 // alunos[alunos.length] = "Luana";
 
-// alunos[0] = "Eduardo";
+// alunos[0] = "Eduardo"; // Altera
 // alunos[3] = "Luiza";
 
 // console.log(alunos);
