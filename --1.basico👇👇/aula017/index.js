5a6225cd3a487c4ac5f8901dda18647bb80f59b4
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-14 00:45:32 -0300

    Arrays --> Adicinando novos elementos pelo tamanho do array

--- --1.basico👇👇/aula017/index.js
+++ --1.basico👇👇/aula017/index.js
@@ -1,8 +1,13 @@
 const alunos = ["Luiz", "Maria", "João"];
 
-alunos[0] = "Eduardo";
-alunos[3] = "Luiza";
+alunos[alunos.length] = "Luiza";
+alunos[alunos.length] = "Fabio";
+alunos[alunos.length] = "Luana";
+
 console.log(alunos);
+
+// alunos[0] = "Eduardo";
+// alunos[3] = "Luiza";
 
 // console.log(alunos);
 // console.log(alunos[0]);
