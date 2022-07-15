d111db90c5f1bcbfc3e954177426d706a14e1b31
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-15 17:05:25 -0300

    Comparando se um numero é igual a

--- --2.Lógica de Programação👇👇/aula021/index.js
+++ --2.Lógica de Programação👇👇/aula021/index.js
@@ -9,7 +9,28 @@
  * != Diferente
  * !== Diferente estrito (valor e tipo)
  */
+// // > Maior que,
+// const num1 = 10;
+// const num2 = 5;
+// const comp = num1 > num2;
+// console.log(comp);
+// // >=Maior que ou igual a,
+// const num1 = 10;
+// const num2 = 5;
+// const comp = num1 >= num2;
+// console.log(comp);
+// // < Menor que,
+// const num1 = 10;
+// const num2 = 11;
+// const comp = num1 < num2;
+// console.log(comp);
+// // <=Menor que ou igual a,
+// const num1 = 10;
+// const num2 = 11;
+// const comp = num1 <= num2;
+// console.log(comp);
+// == Igualdade
 const num1 = 10;
-const num2 = 11;
-const comp = num1 <= num2;
+const num2 = "10";
+const comp = num1 == num2;
 console.log(comp);
