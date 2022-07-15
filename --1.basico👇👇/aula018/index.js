d6f0ac4d1cc01a2594a0ba582ae19b984c72d325
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-14 23:09:12 -0300

    Simplificando a Arrow function

--- --1.basico👇👇/aula018/index.js
+++ --1.basico👇👇/aula018/index.js
@@ -21,9 +21,7 @@ function soma(x = 1, y = 1) {
 //   return n ** 0.5;
 // };
 
-const raiz = (n) => {
-  return n ** 0.5;
-};
+const raiz = (n) => n ** 0.5;
 
 console.log(raiz(9));
 console.log(raiz(16));
