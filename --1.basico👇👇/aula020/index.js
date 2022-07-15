a2ad36b46c56f755d10bd10f2d74823682b887be
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-15 00:44:47 -0300

    Criado uma copia do valor

---
+++ --1.basico👇👇/aula020/index.js
@@ -0,0 +1,14 @@
+/**
+ * Primitivos (imutáveis) - string, number, boolean, undefined null (bigint, symbol)
+ */
+
+/*
+let nome = "Luiz";
+nome[0] = "R"; // não ocorre nada
+console.log(nome[0], nome);
+
+*/
+
+let a = "A";
+let b = a; // Cópia
+console.log(a, b);
