76d83131d9cd7272cf811e6ab3456e6f000f8f91
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-15 17:07:51 -0300

    Comparando se um numero é diferente que

--- --2.Lógica de Programação👇👇/aula021/index.js
+++ --2.Lógica de Programação👇👇/aula021/index.js
@@ -9,26 +9,26 @@
  * != Diferente
  * !== Diferente estrito (valor e tipo)
  */
-// // > Maior que,
-// const num1 = 10;
-// const num2 = 5;
-// const comp = num1 > num2;
-// console.log(comp);
-// // >=Maior que ou igual a,
-// const num1 = 10;
-// const num2 = 5;
-// const comp = num1 >= num2;
-// console.log(comp);
-// // < Menor que,
-// const num1 = 10;
-// const num2 = 11;
-// const comp = num1 < num2;
-// console.log(comp);
-// // <=Menor que ou igual a,
-// const num1 = 10;
-// const num2 = 11;
-// const comp = num1 <= num2;
-// console.log(comp);
+// > Maior que,
+const num1 = 10;
+const num2 = 5;
+const comp = num1 > num2;
+console.log(comp);
+// >=Maior que ou igual a,
+const num1 = 10;
+const num2 = 5;
+const comp = num1 >= num2;
+console.log(comp);
+// < Menor que,
+const num1 = 10;
+const num2 = 11;
+const comp = num1 < num2;
+console.log(comp);
+// <=Menor que ou igual a,
+const num1 = 10;
+const num2 = 11;
+const comp = num1 <= num2;
+console.log(comp);
 // == Igualdade
 const num1 = 10;
 const num2 = "10";
@@ -38,4 +38,9 @@ console.log(comp);
 const num1 = 10;
 const num2 = "10";
 const comp = num1 === num2;
+console.log(comp);
+// != Diferente
+const num1 = 10;
+const num2 = "10";
+const comp = num1 != num2;
 console.log(comp);
