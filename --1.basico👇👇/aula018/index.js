93d7a0b579b5cdd614dd12812d428911dc3d777e
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-14 22:53:12 -0300

    Criando nova função que faz somas

--- --1.basico👇👇/aula018/index.js
+++ --1.basico👇👇/aula018/index.js
@@ -1,6 +1,11 @@
-function saudacao(nome) {
-  return `Bom Dia ${nome}!`;
-}
+// function saudacao(nome) {
+//   return `Bom Dia ${nome}!`;
+// }
 
-const variavel = saudacao("Leo");
-console.log(variavel);
+// const variavel = saudacao("Leo");
+// console.log(variavel);
+
+function soma(x, y) {
+  const resultado = x + y;
+  return resultado;
+}
