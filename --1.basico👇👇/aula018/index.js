953b466d2f1148127794801a8acaa27c90f5e875
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-14 23:06:37 -0300

    Usando nova função

--- --1.basico👇👇/aula018/index.js
+++ --1.basico👇👇/aula018/index.js
@@ -14,9 +14,13 @@ function soma(x = 1, y = 1) {
 // console.log(soma(3, 6));
 // console.log(soma(5, 10));
 
-const resultado = soma(4, 2);
-console.log(resultado);
+// const resultado = soma(4, 2);
+// console.log(resultado);
 
 const raiz = function (n) {
   return n ** 0.5;
 };
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
