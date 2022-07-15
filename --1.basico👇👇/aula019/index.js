dd2e9b1a55661978d1d6a491f94c00f4962f65ca
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-14 23:28:07 -0300

    Objetos --> Criado uma func para criar pessoas

--- --1.basico👇👇/aula019/index.js
+++ --1.basico👇👇/aula019/index.js
@@ -1,14 +1,22 @@
-const pessoa1 = {
-  nome: `Luiz`,
-  sobrenome: `Miranda`,
-  idade: 25,
-};
-const pessoa2 = {
-  nome: `Maria`,
-  sobrenome: `Oliveira`,
-  idade: 55,
-};
+// const pessoa1 = {
+//   nome: `Luiz`,
+//   sobrenome: `Miranda`,
+//   idade: 25,
+// };
+// const pessoa2 = {
+//   nome: `Maria`,
+//   sobrenome: `Oliveira`,
+//   idade: 55,
+// };
 
-console.log(pessoa1.nome);
-console.log(pessoa1.sobrenome);
-console.log(pessoa1.idade);
+// console.log(pessoa1.nome);
+// console.log(pessoa1.sobrenome);
+// console.log(pessoa1.idade);
+
+function criaPessoa(nome, sobrenome, idade) {
+  return {
+    nome: nome,
+    sobrenome: sobrenome,
+    idade: idade,
+  };
+}
