4325423a2a6940ddfc4768303e11afc024cc05f8
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-15 16:11:21 -0300

    Outra maneira de previnir eventos padrão

--- --1.basico👇👇/aula020/ex/js/script.js
+++ --1.basico👇👇/aula020/ex/js/script.js
@@ -1,10 +1,19 @@
 function meuEscopo() {
   const form = document.querySelector(".form");
 
-  form.onsubmit = function (evento) {
-    evento.preventDefault();
-    alert(1);
-    console.log("Foi Enviado");
-  };
+  // form.onsubmit = function (evento) {
+  //   evento.preventDefault();
+  //   alert(1);
+  //   console.log("Foi Enviado");
+  // };
+
+  let contador = 1;
+  function recebeEventoForm(e) {
+    e.preventDefault();
+    console.log(`Form não foi enviado ${contador}`);
+    ++contador;
+  }
+
+  form.addEventListener("submit", recebeEventoForm);
 }
 meuEscopo();
