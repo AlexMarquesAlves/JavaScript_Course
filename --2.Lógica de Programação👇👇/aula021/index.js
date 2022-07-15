0656a51f90cf716079635accd5aeb35dad5c7a06
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-15 17:08:45 -0300

    Comparando se um numero é diferente estritamente

--- --2.Lógica de Programação👇👇/aula021/index.js
+++ --2.Lógica de Programação👇👇/aula021/index.js
@@ -44,3 +44,8 @@ const num1 = 10;
 const num2 = "10";
 const comp = num1 != num2;
 console.log(comp);
+// !== Diferente estrito (valor e tipo)
+const num1 = 10;
+const num2 = "10";
+const comp = num1 !== num2;
+console.log(comp);
