bc27f8441194e8f68c3d12428a028d6a48cd3845
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-15 16:52:05 -0300

    Operadores de comparação

---
+++ --2.Lógica de Programação👇👇/aula021/index.js
@@ -0,0 +1,11 @@
+/**
+ * Operadores de comparação
+ * > Maior que,
+ * >=Maior que ou igual a,
+ * < Menor que,
+ * <=Menor que ou igual a,
+ * == Igualdade
+ * === Igualdade estrita (valor e tipo)
+ * != Diferente
+ * !== Diferente estrito (valor e tipo)
+ */
