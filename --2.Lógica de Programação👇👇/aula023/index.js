5752dde3b52ff3384167847dc374ecbc5e93f9cb
¡Duque <atanaelleonardo@gmail.com>
2022-09-04 21:08:35 -0300

    if, else if & else

---
+++ --2.Lógica de Programação👇👇/aula023/index.js
@@ -0,0 +1,14 @@
+/** if, else if & else
+ */
+
+const numero = 10;
+
+if (numero >= 0 && numero <= 5) {
+  console.log("El numero está entre 0 y 5.");
+} else if (numero >= 6 && numero <= 8) {
+  console.log("El numero está entre 6 y 8.");
+} else if (numero >= 9 && numero <= 11) {
+  console.log("El numero está entre 9 y 11.");
+} else {
+  console.log("El numero no está entre 0 y 11.");
+}
