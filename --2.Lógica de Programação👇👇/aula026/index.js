0eff144a1ef654d074256058447780038de266ed
¡Duque <atanaelleonardo@gmail.com>
2022-09-05 12:57:07 -0300

    Exemplo de objectData 1

---
+++ --2.Lógica de Programação👇👇/aula026/index.js
@@ -0,0 +1,7 @@
+// Object Data
+function objectDateOne() {
+  const tresHoras = 60 * 60 * 3 * 1000;
+  const umDia = 60 * 60 * 24 * 1000;
+  const data = new Date(0 + tresHoras + umDia);
+  console.log(data.toString());
+}
