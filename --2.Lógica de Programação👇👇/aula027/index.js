3ce4c9c018ad830f73e25cb5c5916e556ea83cca
¡Duque <atanaelleonardo@gmail.com>
2022-09-05 19:32:04 -0300

    Renomeando funcao e usando Switch

--- --2.Lógica de Programação👇👇/aula027/index.js
+++ --2.Lógica de Programação👇👇/aula027/index.js
@@ -1,25 +1,37 @@
 // Switch | Case
-
-function objectDate(data) {
-  const data = new Date("1987-04-21 00:00:00");
-  const diaSemana = data.getDay();
+function getDayWeek() {
   let diaSemanaTexto;
 
-  if (diaSemanaTexto === 0) {
-    diaSemanaTexto = "Domingo";
-  } else if (diaSemanaTexto === 1) {
-    diaSemanaTexto = "Segunda";
-  } else if (diaSemanaTexto === 2) {
-    diaSemanaTexto = "Terça";
-  } else if (diaSemanaTexto === 3) {
-    diaSemanaTexto = "Quarta";
-  } else if (diaSemanaTexto === 4) {
-    diaSemanaTexto = "Quinta";
-  } else if (diaSemanaTexto === 5) {
-    diaSemanaTexto = "Sexta";
-  } else if (diaSemanaTexto === 6) {
-    diaSemanaTexto = "Sábado";
-  } else {
-    diaSemanaTexto = ``;
+  switch (diaSemana) {
+    case 0:
+      diaSemanaTexto = "Domingo";
+      return diaSemanaTexto;
+    case 1:
+      diaSemanaTexto = "Segunda";
+      return diaSemanaTexto;
+    case 2:
+      diaSemanaTexto = "Terça";
+      return diaSemanaTexto;
+    case 3:
+      diaSemanaTexto = "Quarta";
+      return diaSemanaTexto;
+    case 4:
+      diaSemanaTexto = "Quinta";
+      return diaSemanaTexto;
+    case 5:
+      diaSemanaTexto = "Sexta";
+      return diaSemanaTexto;
+    case 6:
+      diaSemanaTexto = "Sábado";
+      return diaSemanaTexto;
+    default:
+      diaSemanaTexto = ``;
+      return diaSemanaTexto;
   }
 }
+
+const data = new Date("1987-04-21 00:00:00");
+const diaSemana = data.getDay();
+const diaSemanaTexto = getDayWeek(diaSemana);
+
+console.log(diaSemana, diaSemanaTexto);
