17f09095397c9b8b2c1ec9b3afeeb605c81c4f5a
¡Duque <atanaelleonardo@gmail.com>
2022-09-06 16:21:26 -0300

    🆕 constant added

---
+++ --2.Lógica de Programação👇👇/aula035/assets/js/main.js
@@ -0,0 +1,15 @@
+const elements = [
+  {
+    tag: "p",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus",
+  },
+  {
+    tag: "div",
+    text: "Nulla repellat voluptatum quibusdam, commodi explicabo sapiente eveniet culpa quod assumenda minima labore quo provident eum eius sed. Nulla repellat voluptatum quibusdam, commodi explicabo sapiente eveniet culpa quod assumenda minima labore quo provident eum eius sed.",
+  },
+  {
+    tag: "section",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto cum, quo possimus laboriosam vitae vero iste nobis distinctio asperiores voluptate itaque ea exercitationem. Quis, sapiente dolorem quas consequatur velit maxime!  Ipsa dolorum doloribus quae praesentium ut? Quisquam quae eius voluptas accusantium hic deserunt sunt repellendus esse voluptates cupiditate aliquam, amet dolore cum ipsum iusto natus fugit nostrum repudiandae facere fuga?  Cupiditate quidem aspernatur iusto excepturi ab? Tempore dolorum, praesentium doloribus laborum placeat, quis quaerat enim aspernatur ipsum atque iste! Exercitationem ratione ipsa debitis porro qui temporibus, velit vitae voluptate? Quaerat.",
+  },
+  { tag: "footer", text: "Dolor sit amet" },
+];
