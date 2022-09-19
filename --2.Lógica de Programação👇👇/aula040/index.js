97b53a9d85a752b59c3c6976550b11dedf1c4858
¡Duque <atanaelleonardo@gmail.com>
2022-09-19 02:05:23 -0300

    Laços de Repetição { Break }

--- --2.Lógica de Programação👇👇/aula040/index.js
+++ --2.Lógica de Programação👇👇/aula040/index.js
@@ -1,8 +1,12 @@
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 for (const numero of numeros) {
-  if (numero === 2) {
+  if (numero === 2 || numero === 5) {
     continue;
+  }
+
+  if (numero === 7) {
+    break;
   }
   console.log(numero);
 }
