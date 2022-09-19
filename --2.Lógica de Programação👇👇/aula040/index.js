0c51188f9ea02d6b8b2fb705e8fbfb425cc79cbf
¡Duque <atanaelleonardo@gmail.com>
2022-09-19 02:12:47 -0300

    Break e Continue

--- --2.Lógica de Programação👇👇/aula040/index.js
+++ --2.Lógica de Programação👇👇/aula040/index.js
@@ -1,12 +1,80 @@
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+// For of
 for (const numero of numeros) {
   if (numero === 2 || numero === 5) {
     continue;
   }
 
+  console.log(numero);
   if (numero === 7) {
+    console.log("Saindo...");
     break;
   }
-  console.log(numero);
 }
+// For in
+for (let i in numeros) {
+  let numero = numeros[i];
+
+  if (numero === 2 || numero === 5) {
+    continue;
+  }
+
+  console.log(numero);
+  if (numero === 7) {
+    console.log("Saindo...");
+    break;
+  }
+}
+// For Classic
+for (let i = 0; i < numeros.length; i++) {
+  let numero = numeros[i];
+
+  if (numero === 2 || numero === 5) {
+    continue;
+  }
+
+  console.log(numero);
+  if (numero === 7) {
+    console.log("Saindo...");
+    break;
+  }
+}
+// While
+let i = 0;
+while (i < numeros.length) {
+  let numero = numeros[i];
+
+  if (numero === 2 || numero === 5) {
+    i++;
+    continue;
+  }
+
+  console.log(numero);
+  if (numero === 7) {
+    console.log("Saindo...");
+    i++;
+    break;
+  }
+
+  i++;
+}
+// Do While
+let i = 0;
+do {
+  let numero = numeros[i];
+
+  if (numero === 2 || numero === 5) {
+    i++;
+    continue;
+  }
+
+  console.log(numero);
+  if (numero === 7) {
+    console.log("Saindo...");
+    i++;
+    break;
+  }
+
+  i++;
+} while (i < numeros.length);
