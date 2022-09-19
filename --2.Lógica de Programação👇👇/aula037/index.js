c3e4a35fbc4aee07a5ed171325281ee836fd4ee1
¡Duque <atanaelleonardo@gmail.com>
2022-09-19 00:48:05 -0300

    For of - Estrutura de repetição

--- --2.Lógica de Programação👇👇/aula037/index.js
+++ --2.Lógica de Programação👇👇/aula037/index.js
@@ -1,14 +1,33 @@
-const nome = ["Luiz", "Otávio", "Henrique"];
+const nomes = ["Luiz", "Otávio", "Henrique"];
 
 // For Classic
-for (let i = 0; i < nome.length; i++) {
-  console.log(nome[i]);
+for (let i = 0; i < nomes.length; i++) {
+  console.log(nomes[i]);
 }
+console.log("\n");
 // For in
-for (let i in nome) {
-  console.log(nome[i]);
+for (let i in nomes) {
+  console.log(nomes[i]);
 }
+console.log("\n");
 // For of
-for (const valor of nome) {
+for (const valor of nomes) {
   console.log(valor);
+}
+console.log("\n");
+
+nomes.forEach(function (valor, indice) {
+  console.log(valor, indice);
+});
+
+console.log("--------------------------------------------------------");
+
+const pessoa = {
+  nome: "Luiz",
+  sobrenome: "Otávio",
+  idade: 30,
+};
+
+for (let key in pessoa) {
+  console.log(key, pessoa[i]);
 }
