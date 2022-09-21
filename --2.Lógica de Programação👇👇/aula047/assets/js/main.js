5de9aa6651f891a8c0dbb8fcb49ea6f12bbcbaa8
¡Duque <atanaelleonardo@gmail.com>
2022-09-20 22:32:03 -0300

    🆕 code cleaning

--- --2.Lógica de Programação👇👇/aula047/assets/js/main.js
+++ --2.Lógica de Programação👇👇/aula047/assets/js/main.js
@@ -13,9 +13,7 @@ function getTimeFromSeconds(seconds) {
 }
 
 const relogio = document.querySelector(`.relogio`);
-const iniciar = document.querySelector(`.iniciar`);
-const pausar = document.querySelector(`.pausar`);
-const zerar = document.querySelector(`.zerar`);
+
 let seconds = 0;
 let timer;
 
