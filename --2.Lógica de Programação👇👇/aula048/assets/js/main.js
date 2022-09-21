ec8766300687756d14086d046419130405cc7f0a
¡Duque <atanaelleonardo@gmail.com>
2022-09-21 11:38:35 -0300

    🆕 function that create a new todo

--- --2.Lógica de Programação👇👇/aula048/assets/js/main.js
+++ --2.Lógica de Programação👇👇/aula048/assets/js/main.js
@@ -2,7 +2,16 @@ const inputTarefa = document.querySelector(".input-tarefa");
 const btnTarefa = document.querySelector(".btn-tarefa");
 const tarefas = document.querySelector(".tarefas");
 
+function createTodo(textoInput) {
+  // const li = criaLi();
+  li.innerText = textoInput;
+  tarefas.appendChild(li);
+  // limpaInput();
+  // criaBotaoApagar(li);
+  // salvarTarefas();
+}
+
 btnTarefa.addEventListener("click", function () {
   if (!inputTarefa.value) return;
-  criaTarefa(inputTarefa.value);
+  createTodo(inputTarefa.value);
 });
