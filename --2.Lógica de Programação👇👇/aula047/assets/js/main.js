a997e1bbf89a644fcd669ee4318e7303a30aa8e4
¡Duque <atanaelleonardo@gmail.com>
2022-09-20 22:01:02 -0300

    🆕 Capturando eventos de clique

--- --2.Lógica de Programação👇👇/aula047/assets/js/main.js
+++ --2.Lógica de Programação👇👇/aula047/assets/js/main.js
@@ -2,3 +2,15 @@ const relogio = document.querySelector(`.relogio`);
 const iniciar = document.querySelector(`.iniciar`);
 const pausar = document.querySelector(`.pausar`);
 const zerar = document.querySelector(`.zerar`);
+
+iniciar.addEventListener(`click`, (event) => {
+  alert(`Cliquei no iniciar`);
+});
+
+pausar.addEventListener(`click`, (event) => {
+  alert(`Cliquei no pausar`);
+});
+
+zerar.addEventListener(`click`, (event) => {
+  alert(`Cliquei no zerar`);
+});
