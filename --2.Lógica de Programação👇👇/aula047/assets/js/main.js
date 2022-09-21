f6a9a4a0ff2df3e57c09ee9ea75b4af5c3b1fdc4
¡Duque <atanaelleonardo@gmail.com>
2022-09-20 22:34:15 -0300

    🆕 Removing functions from global scope

--- --2.Lógica de Programação👇👇/aula047/assets/js/main.js
+++ --2.Lógica de Programação👇👇/aula047/assets/js/main.js
@@ -1,47 +1,50 @@
-// new Date(0);
+function virtualWatch() {
+  // new Date(0);
+  function getTimeFromSeconds(seconds) {
+    const data = new Date(seconds * 1000);
 
-function getTimeFromSeconds(seconds) {
-  const data = new Date(seconds * 1000);
+    return data.toLocaleTimeString("pt-BR", {
+      hour: "2-digit",
+      hour12: false,
+      minute: "2-digit",
+      second: "2-digit",
+      timeZone: `GMT`,
+    });
+  }
 
-  return data.toLocaleTimeString("pt-BR", {
-    hour: "2-digit",
-    hour12: false,
-    minute: "2-digit",
-    second: "2-digit",
-    timeZone: `GMT`,
+  const relogio = document.querySelector(`.relogio`);
+
+  let seconds = 0;
+  let timer;
+
+  function startWatch() {
+    timer = setInterval(() => {
+      seconds++;
+      relogio.innerHTML = getTimeFromSeconds(seconds);
+    }, 1000);
+  }
+
+  document.addEventListener(`click`, (e) => {
+    const el = e.target;
+
+    if (el.classList.contains(`zerar`)) {
+      relogio.classList.remove(`pausado`);
+      clearInterval(timer);
+      relogio.innerHTML = `00:00:00`;
+      seconds = 0;
+    }
+
+    if (el.classList.contains(`iniciar`)) {
+      relogio.classList.remove(`pausado`);
+      clearInterval(timer);
+      startWatch();
+    }
+
+    if (el.classList.contains(`pausar`)) {
+      clearInterval(timer);
+      relogio.classList.add(`pausado`);
+    }
   });
 }
 
-const relogio = document.querySelector(`.relogio`);
-
-let seconds = 0;
-let timer;
-
-function startWatch() {
-  timer = setInterval(() => {
-    seconds++;
-    relogio.innerHTML = getTimeFromSeconds(seconds);
-  }, 1000);
-}
-
-document.addEventListener(`click`, (e) => {
-  const el = e.target;
-
-  if (el.classList.contains(`zerar`)) {
-    relogio.classList.remove(`pausado`);
-    clearInterval(timer);
-    relogio.innerHTML = `00:00:00`;
-    seconds = 0;
-  }
-
-  if (el.classList.contains(`iniciar`)) {
-    relogio.classList.remove(`pausado`);
-    clearInterval(timer);
-    startWatch();
-  }
-
-  if (el.classList.contains(`pausar`)) {
-    clearInterval(timer);
-    relogio.classList.add(`pausado`);
-  }
-});
+virtualWatch();
