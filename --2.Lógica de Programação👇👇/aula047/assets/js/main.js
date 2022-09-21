933872c19bb769d7a6e7c875868f24f10022bff4
¡Duque <atanaelleonardo@gmail.com>
2022-09-20 22:06:46 -0300

    🆕 seconds variable created

--- --2.Lógica de Programação👇👇/aula047/assets/js/main.js
+++ --2.Lógica de Programação👇👇/aula047/assets/js/main.js
@@ -16,6 +16,7 @@ const relogio = document.querySelector(`.relogio`);
 const iniciar = document.querySelector(`.iniciar`);
 const pausar = document.querySelector(`.pausar`);
 const zerar = document.querySelector(`.zerar`);
+let seconds = 0;
 
 iniciar.addEventListener(`click`, (event) => {
   alert(`Cliquei no iniciar`);
