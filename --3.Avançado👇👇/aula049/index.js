af9862a79472ae4bf54e72bb2cf329fcfc449623
¡Duque <atanaelleonardo@gmail.com>
2022-09-21 12:12:20 -0300

    🆕 First-class objects

--- --3.Avançado👇👇/aula049/index.js
+++ --3.Avançado👇👇/aula049/index.js
@@ -4,3 +4,16 @@ falaOi();
 function falaOi() {
   console.log("Oie");
 }
+
+// First-class objects (Objetos de primeira classe)
+// Function expression
+const souUmDado = function () {
+  console.log("Sou um dado.");
+};
+
+function executaFuncao(funcao) {
+  console.log("vou executar sua função abaixo");
+  funcao();
+}
+
+executaFuncao(souUmDado);
