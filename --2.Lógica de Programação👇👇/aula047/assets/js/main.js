e74ff3130a5de880b4fc9ee6b55a09de32e73411
¡Duque <atanaelleonardo@gmail.com>
2022-09-20 22:06:10 -0300

    🆕 function to get seconds of time

--- --2.Lógica de Programação👇👇/aula047/assets/js/main.js
+++ --2.Lógica de Programação👇👇/aula047/assets/js/main.js
@@ -1,3 +1,17 @@
+// new Date(0);
+
+function getTimeFromSeconds(seconds) {
+  const data = new Date(seconds * 1000);
+
+  return data.toLocaleTimeString("pt-BR", {
+    hour: "2-digit",
+    hour12: false,
+    minute: "2-digit",
+    second: "2-digit",
+    timeZone: `GMT`,
+  });
+}
+
 const relogio = document.querySelector(`.relogio`);
 const iniciar = document.querySelector(`.iniciar`);
 const pausar = document.querySelector(`.pausar`);
