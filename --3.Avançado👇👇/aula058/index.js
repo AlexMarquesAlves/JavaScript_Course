39b4c2b12cd45f71e6ddc6a3909d0475897b86d0
¡Duque <atanaelleonardo@gmail.com>
2022-09-29 00:53:54 -0300

    🆕 Exemplo de Atributos Privados

--- --3.Avançado👇👇/aula058/index.js
+++ --3.Avançado👇👇/aula058/index.js
@@ -3,6 +3,11 @@
 // Construtora => Sempre iniciada com letra maiúscula... (new) 'Constructor'
 
 function Pessoa(nome, sobrenome) {
+  // Atributos Privados
+  const ID = 123456;
+
+  const metodoInterno = function () {};
+
   this.nome = nome;
   this.sobrenome = sobrenome;
 
@@ -13,3 +18,5 @@ function Pessoa(nome, sobrenome) {
 
 const p1 = new Pessoa("Luiz", "Otávio");
 const p1 = new Pessoa("Maria", "Oliveira");
+
+p2.metodo();
