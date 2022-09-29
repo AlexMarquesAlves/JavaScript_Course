535df6bf1a960dd7f85a081dd3c33112709b1d87
¡Duque <atanaelleonardo@gmail.com>
2022-09-29 01:24:31 -0300

    🆕 Método que limpa o display

--- --3.Avançado👇👇/aula059/assets/js/main.js
+++ --3.Avançado👇👇/aula059/assets/js/main.js
@@ -1,76 +1,3 @@
-// function criaCalculadora() {
-//   return {
-//     // Atributos
-//     display: document.querySelector(".display"),
-
-//     start() {
-//       this.buttonClicks();
-//       this.pressEnter();
-//     },
-
-//     // Métodos
-//     buttonClicks() {
-//       document.addEventListener("click", (e) => {
-//         const el = e.target;
-
-//         if (el.classList.contains("btn-num")) {
-//           this.btnToDisplay(el.innerText);
-//         }
-
-//         if (el.classList.contains("btn-clear")) {
-//           this.clearDisplay();
-//         }
-
-//         if (el.classList.contains("btn-del")) {
-//           this.backSpace();
-//         }
-
-//         if (el.classList.contains("btn-eq")) {
-//           this.haceCuenta();
-//         }
-//       });
-//     },
-
-//     btnToDisplay(valor) {
-//       this.display.value += valor;
-//     },
-
-//     clearDisplay() {
-//       this.display.value = ``;
-//     },
-
-//     backSpace() {
-//       this.display.value = this.display.value.slice(0, -1);
-//     },
-
-//     pressEnter() {
-//       this.display.addEventListener("keyup", (e) => {
-//         if (e.keyCode === 13) {
-//           this.haceCuenta();
-//         }
-//       });
-//     },
-
-//     haceCuenta() {
-//       let conta = this.display.value;
-
-//       try {
-//         conta = eval(conta);
-
-//         if (!conta) {
-//           alert(`Conta inválida`);
-//           return;
-//         }
-
-//         this.display.value = String(conta);
-//       } catch (e) {
-//         alert(`Conta inválida`);
-//         return;
-//       }
-//     },
-//   };
-// }
-
 function Calculadora() {
   this.display = document.querySelector(`.display`);
 
@@ -81,11 +8,17 @@ function Calculadora() {
       if (el.classList.contains("btn-num")) {
         this.addNumDisplay(el);
       }
+      if (el.classList.contains("btn-clear")) {
+        this.clear();
+      }
     });
   };
 
   this.addNumDisplay = (el) => {
     this.display.value += el.innerText;
+  };
+  this.clear = () => {
+    this.display.value = ``;
   };
 
   this.start = () => {
