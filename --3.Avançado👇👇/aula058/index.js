d156907f4e25b39aaa05a5c5904441629855b559
¡Duque <atanaelleonardo@gmail.com>
2022-09-29 00:51:43 -0300

    🆕 exemplo de método na função construtora

--- --3.Avançado👇👇/aula058/index.js
+++ --3.Avançado👇👇/aula058/index.js
@@ -5,6 +5,10 @@
 function Pessoa(nome, sobrenome) {
   this.nome = nome;
   this.sobrenome = sobrenome;
+
+  this.metodo = function () {
+    console.log(`${this.nome}: sou um método`);
+  };
 }
 
 const p1 = new Pessoa("Luiz", "Otávio");
