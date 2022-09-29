1ab862716792020a17e425efc69edadde9e0e7d3
¡Duque <atanaelleonardo@gmail.com>
2022-09-29 01:59:47 -0300

    🆕 Exemplo 3, 4 de função geradora

--- --3.Avançado👇👇/aula061/index.js
+++ --3.Avançado👇👇/aula061/index.js
@@ -40,3 +40,22 @@ console.log(g2.next().value);
 console.log(g2.next().value);
 
 // ----------------------------------------------------
+console.log(`----------------------------------------------------`);
+
+function* geradora3() {
+  yield 0;
+  yield 1;
+  yield 2;
+}
+
+function* geradora4() {
+  yield* geradora3();
+  yield 3;
+  yield 4;
+  yield 5;
+}
+
+const g4 = geradora4();
+for (const valor of g4) {
+  console.log(valor);
+}
