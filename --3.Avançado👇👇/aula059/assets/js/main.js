c5cb67905812e8fce838da1c2d17f6130f169db5
¡Duque <atanaelleonardo@gmail.com>
2022-09-29 01:37:02 -0300

    🆕 Code refactor

--- --3.Avançado👇👇/aula059/assets/js/main.js
+++ --3.Avançado👇👇/aula059/assets/js/main.js
@@ -19,18 +19,10 @@ function Calculadora() {
     document.addEventListener(`click`, (event) => {
       const el = event.target;
 
-      if (el.classList.contains("btn-num")) {
-        this.addNumDisplay(el);
-      }
-      if (el.classList.contains("btn-clear")) {
-        this.clear();
-      }
-      if (el.classList.contains("btn-del")) {
-        this.del();
-      }
-      if (el.classList.contains("btn-eq")) {
-        this.resolve();
-      }
+      if (el.classList.contains("btn-num")) this.addNumDisplay(el);
+      if (el.classList.contains("btn-clear")) this.clear();
+      if (el.classList.contains("btn-del")) this.del();
+      if (el.classList.contains("btn-eq")) this.resolve();
     });
   };
 
@@ -52,12 +44,8 @@ function Calculadora() {
     this.display.value += el.innerText;
     this.display.focus();
   };
-  this.clear = () => {
-    this.display.value = ``;
-  };
-  this.del = () => {
-    this.display.value = this.display.slice(0, -1);
-  };
+  this.clear = () => (this.display.value = ``);
+  this.del = () => (this.display.value = this.display.slice(0, -1));
 }
 
 const calculadora = new Calculadora();
