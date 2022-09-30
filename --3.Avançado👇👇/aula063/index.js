66aadbf3deb1c61a0a705caba9fe563df56247ad
¡Duque <atanaelleonardo@gmail.com>
2022-09-30 20:35:44 -0300

    🆕 Método Splice // pop //

---
+++ --3.Avançado👇👇/aula063/index.js
@@ -0,0 +1,18 @@
+const nomes = ["Maria", "João", "Eduardo", "Gabriel", "Julia"];
+
+//  nomes.splice(indice, delete, elem1, elem2, elem3)
+
+/**
+ *
+ * const removidos = nomes.splice(-2, Number.MAX_VALUE);
+ * console.log(nomes, removidos);
+ * const removidos = nomes.splice(3, 0, "Luiz");
+ * console.log(nomes, removidos);
+ * const removidos = nomes.splice(3, 2, "Luiz", "Otávio");
+ * console.log(nomes, removidos);
+ *
+ */
+
+/* pop */
+const removidos = nomes.splice(-1, 1);
+console.log(nomes, removidos);
