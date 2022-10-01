8b0f70598349bc4e98520a637e753465d2a7f46b
¡Duque <atanaelleonardo@gmail.com>
2022-10-01 20:33:57 -0300

    🆕 Exemplo para filtrar

--- --3.Avançado👇👇/aula065/index.js
+++ --3.Avançado👇👇/aula065/index.js
@@ -12,3 +12,17 @@ const num = [5, 50.8, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
 const numMoreThanTen = num.filter((value) => value > 10);
 console.log(numMoreThanTen);
+
+console.log("----------------------------------------------------------------");
+
+// Retorne as pessoas que tem o nome com 5 letras ou mais
+// Retorne as pessoas com mais de 50 anos
+// Retorne as pessoas cujo nome termina com a
+const pessoas = [
+  { nome: "Luiz", idade: 62 },
+  { nome: "Maria", idade: 23 },
+  { nome: "Eduardo", idade: 55 },
+  { nome: "Letícia", idade: 19 },
+  { nome: "Rosana", idade: 32 },
+  { nome: "Wallace", idade: 47 },
+];
