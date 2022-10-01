25d4292b6c3ac6c4f7c6eb1be5e495ab23899f08
¡Duque <atanaelleonardo@gmail.com>
2022-10-01 20:20:45 -0300

    🆕 Used anonymous function on filter

--- --3.Avançado👇👇/aula065/index.js
+++ --3.Avançado👇👇/aula065/index.js
@@ -3,9 +3,7 @@
 // Retorne os números maiores que 10
 const num = [5, 50.8, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
-function callbackFilter(value) {
+const numMoreThanTen = num.filter(function (value) {
   return value > 10;
-}
-
-const numMoreThanTen = num.filter(callbackFilter);
+});
 console.log(numMoreThanTen);
