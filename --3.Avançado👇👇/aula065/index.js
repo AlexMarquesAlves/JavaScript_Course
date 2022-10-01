59c197a5bdaaa9f2cdd76d4d449c22a6104dafd3
¡Duque <atanaelleonardo@gmail.com>
2022-10-01 20:22:22 -0300

    🆕 Code refactor

--- --3.Avançado👇👇/aula065/index.js
+++ --3.Avançado👇👇/aula065/index.js
@@ -3,7 +3,5 @@
 // Retorne os números maiores que 10
 const num = [5, 50.8, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
-const numMoreThanTen = num.filter((value) => {
-  return value > 10;
-});
+const numMoreThanTen = num.filter((value) => value > 10);
 console.log(numMoreThanTen);
