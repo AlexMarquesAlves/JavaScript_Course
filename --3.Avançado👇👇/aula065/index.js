5232c4c8cb3d9fed7e5d74d40618b0a826033f60
¡Duque <atanaelleonardo@gmail.com>
2022-10-01 20:18:18 -0300

    🆕 Code refactor on callbackFilter function

--- --3.Avançado👇👇/aula065/index.js
+++ --3.Avançado👇👇/aula065/index.js
@@ -3,12 +3,8 @@
 // Retorne os números maiores que 10
 const num = [5, 50.8, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
-function callbackFilter(value, index, array) {
-  if (value > 10) {
-    return true;
-  } else {
-    return false;
-  }
+function callbackFilter(value) {
+  return value > 10;
 }
 
 const numMoreThanTen = num.filter(callbackFilter);
