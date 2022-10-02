da1ae1c697f49d55e745a017a20dd88226bafcff
¡Duque <atanaelleonardo@gmail.com>
2022-10-01 21:21:36 -0300

    🆕 Exemplo I

---
+++ --3.Avançado👇👇/aula067/index.js
@@ -0,0 +1,9 @@
+/**
+ * Reduce ->
+ */
+
+// Some todos os números (Reduce)
+// retorne um array com numeros pares (Filter)
+// Retorne um array com o Dobro dos Valores (Map)
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
