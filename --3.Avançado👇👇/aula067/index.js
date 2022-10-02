2e8f42f1490a0e3507467a37ae6900327f80f3f2
¡Duque <atanaelleonardo@gmail.com>
2022-10-01 21:26:36 -0300

    🆕 Reduce -> Somado valores do array... ✔️

--- --3.Avançado👇👇/aula067/index.js
+++ --3.Avançado👇👇/aula067/index.js
@@ -7,3 +7,8 @@
 // Retorne um array com o Dobro dos Valores (Map)
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const total = numeros.reduce((acumulador, valor) => {
+  acumulador += valor;
+  return acumulador;
+}, 0); // Somado valores do array... ✔️
+console.log(total);
