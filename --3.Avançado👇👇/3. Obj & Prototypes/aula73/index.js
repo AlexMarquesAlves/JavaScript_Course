7b6b2dee68e2c8301e4bec284d376aafb71be9c7
¡Duque <atanaelleonardo@gmail.com>
2022-10-04 00:43:38 -0300

    🆕 Métodos úteis para objetos // Object.entries

---
+++ --3.Avançado👇👇/3. Obj & Prototypes/aula73/index.js
@@ -0,0 +1,23 @@
+/**
+ * Object.values
+ * Object.entries
+ * Object.assign(des, any)
+ * Object.getOwnPropertyDescriptor(o,'prop')
+ * ... (spread)
+ *
+ * // ya hemos visto
+ * Object.keys (retorna as chaves)
+ * Object.freeze (congela o objeto)
+ * Object.defineProperties (Define varias propriedades)
+ * Object.defineProperty (Define uma propriedade)
+ */
+const produto = { nome: "Produto", preco: 1.8 };
+const caneca = {
+  ...produto,
+  material: "porcelana",
+};
+
+caneca.nome = "Outro nome";
+caneca.preco = 2.5;
+console.log(produto);
+console.log(caneca);
