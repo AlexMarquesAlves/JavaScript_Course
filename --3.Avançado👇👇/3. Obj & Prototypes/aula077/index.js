82a9eabe8c79c50182f1d674eadff140e81be9dd
¡Duque <atanaelleonardo@gmail.com>
2022-10-06 01:22:17 -0300

    🆕 prototype método que cria digito

--- --3.Avançado👇👇/3. Obj & Prototypes/aula077/index.js
+++ --3.Avançado👇👇/3. Obj & Prototypes/aula077/index.js
@@ -36,4 +36,20 @@ ValidaCPF.prototype.valida = function () {
   return novoCpf === this.cpfLimpo;
 };
 
+ValidaCPF.prototype.criaDigito = function (cpfParcial) {
+  const cpfArray = Array.from(cpfParcial);
+
+  let regressivo = cpfArray.length + 1;
+  const total = cpfArray.reduce((ac, val) => {
+    ac += regressivo * Number(val);
+    regressivo--;
+    return ac;
+  }, 0);
+
+  const digito = 11 - (total % 11);
+  return digito > 9 ? "0" : String(digito);
+};
+
 const cpf = new ValidaCPF("070.987.720-03");
+console.log(cpf.cpfLimpo);
+cpf.valida();
