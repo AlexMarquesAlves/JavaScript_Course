cd5ab9d39a784ccdfc2526513444f62a42069345
¡Duque <atanaelleonardo@gmail.com>
2022-10-06 01:20:30 -0300

    🆕 Terminado método que valida

--- --3.Avançado👇👇/3. Obj & Prototypes/aula077/index.js
+++ --3.Avançado👇👇/3. Obj & Prototypes/aula077/index.js
@@ -27,6 +27,13 @@ ValidaCPF.prototype.valida = function () {
   if (typeof this.cpfLimpo === "undefined") return false;
   if (this.cpfLimpo.length !== 11) return false;
   if (this.isSequencia()) return false;
+
+  const cpfParcial = this.cpfLimpo.slice(0, -2);
+  const digito1 = this.criaDigito(cpfParcial);
+  const digito2 = this.criaDigito(cpfParcial + digito1);
+
+  const novoCpf = cpfParcial + digito1 + digito2;
+  return novoCpf === this.cpfLimpo;
 };
 
 const cpf = new ValidaCPF("070.987.720-03");
