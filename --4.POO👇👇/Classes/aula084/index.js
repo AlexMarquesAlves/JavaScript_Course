0c85cbbebc11ef3e706557c856cde928825f1a6a
¡Duque <atanaelleonardo@gmail.com>
2022-10-06 21:03:55 -0300

    🆕 Métodos de instância added

--- --4.POO👇👇/Classes/aula084/index.js
+++ --4.POO👇👇/Classes/aula084/index.js
@@ -3,4 +3,18 @@ class ControleRemoto {
     this.tv = tv;
     this.volume = 0;
   }
+
+  // Método de instância
+  aumentarVolume() {
+    this.volume += 2;
+  }
+  diminuirVolume() {
+    this.volume -= 2;
+  }
 }
+
+const controle1 = new ControleRemoto("LG");
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+console.log(controle1);
