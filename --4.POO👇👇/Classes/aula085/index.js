4289da38d6a6909d898e6738709617fa5325a615
¡Duque <atanaelleonardo@gmail.com>
2022-10-07 00:33:28 -0300

    Usando instância estática no método geraDigito

--- --4.POO👇👇/Classes/aula085/index.js
+++ --4.POO👇👇/Classes/aula085/index.js
@@ -16,13 +16,13 @@ class ValidaCPF {
 
   geraNovoCPF() {
     const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
-    const digito1 = this.geraDigito(cpfSemDigitos);
-    const digito2 = this.geraDigito(cpfSemDigitos + digito1);
+    const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
+    const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
 
     this.novoCPF = cpfSemDigitos + digito1 + digito2;
   }
 
-  geraDigito(cpfSemDigitos) {
+  static geraDigito(cpfSemDigitos) {
     let total = 0;
     let reverso = cpfSemDigitos.length + 1;
     for (let stringNumerica of cpfSemDigitos) {
