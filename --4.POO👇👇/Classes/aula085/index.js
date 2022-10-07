98e9d674e9ccc8aa8d045f7d8912b454d1d2291f
¡Duque <atanaelleonardo@gmail.com>
2022-10-06 22:14:31 -0300

    🆕 Classe criada para validar CPF

---
+++ --4.POO👇👇/Classes/aula085/index.js
@@ -0,0 +1,15 @@
+// 705.484.450-52 070.987.720-03
+
+class ValidaCPF {
+  constructor(cpfEnviado) {
+    Object.defineProperty(this, "cpfLimpo", {
+      writable: false,
+      enumerable: true,
+      configurable: false,
+      value: cpfEnviado.replace(/\D+/g, ""),
+    });
+  }
+}
+
+const validacpf = new ValidaCPF(`070.987.720-03`);
+console.log(validacpf.valida());
