c86371a0cb72e8e85e6492adb456cc6896c4391b
¡Duque <atanaelleonardo@gmail.com>
2022-10-07 01:08:58 -0300

    🆕 Maneira que será exibido o erro criado

--- --4.POO👇👇/Classes/aula086/assets/js/main.js
+++ --4.POO👇👇/Classes/aula086/assets/js/main.js
@@ -21,12 +21,16 @@ class ValidaFormulario {
 
     for (const campo of this.formulario.querySelector(".validar")) {
       if (!campo.value) {
-        this.criaErro();
+        this.criaErro(campo, `O campo ${campo} não pode está em branco`);
       }
     }
   }
 
-  criaErro(campo, msg) {}
+  criaErro(campo, msg) {
+    const div = document.createElement("div");
+    div.innerHTML = msg;
+    div.classList.add("error-text");
+  }
 }
 
 const valida = new ValidaFormulario();
