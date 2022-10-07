dcc912e6cd9047cc4243964b3d59eb1f02c56899
¡Duque <atanaelleonardo@gmail.com>
2022-10-06 22:01:43 -0300

    🆕 Método estático

--- --4.POO👇👇/Classes/aula084/index.js
+++ --4.POO👇👇/Classes/aula084/index.js
@@ -11,10 +11,12 @@ class ControleRemoto {
   diminuirVolume() {
     this.volume -= 2;
   }
+
+  // Método estático
+  static soma(x, y) {
+    console.log(this);
+  }
 }
 
 const controle1 = new ControleRemoto("LG");
-controle1.aumentarVolume();
-controle1.aumentarVolume();
-controle1.aumentarVolume();
-console.log(controle1);
+ControleRemoto.soma();
