94cc4bf473aaf66a81358f85351dbafe16d56c5e
¡Duque <atanaelleonardo@gmail.com>
2022-10-10 02:21:51 -0300

    Criando um gerador de CPFs válidos

--- --6.Tooling ES6👇👇/aula096/src/main.js
+++ --6.Tooling ES6👇👇/aula096/src/main.js
@@ -1,1 +1,9 @@
 import "./assets/css/style.css";
+import "./assets/css/styles.css";
+import GeraCPF from "./modules/GeraCPF";
+
+(function () {
+  const gera = new GeraCPF();
+  const cpfGerado = document.querySelector(".cpf-gerado");
+  cpfGerado.innerHTML = gera.geraNovoCpf();
+})();
