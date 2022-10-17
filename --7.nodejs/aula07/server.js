0ac8a977305afcd7333e5b8aae753d1bc6b40a3b
¡Duque <atanaelleonardo@gmail.com>
2022-10-17 18:12:07 -0300

    Express - req.params, req.query e req.body

--- --7.nodejs/aula07/server.js
+++ --7.nodejs/aula07/server.js
@@ -1,31 +1,30 @@
 const express = require("express");
 const app = express();
 
-/**
- *         Criar  ||  Ler  || Atualizar || Apagar
- * CRUD => CREATE,||  READ,|| UPDATE,   || DELETE
- *         POST   ||  GET  || PUT       || DELETE
- */
-
-// http://meusite.com/ <- GET -> Entregue a página /
-// http://meusite.com/sobre <- GET -> Entregue a página /sobre
-// http://meusite.com/contato <- GET -> Entregue a página /contato
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send(`
   <form action="/" method="POST">
-  Nome do cliente: <input type="text" name="nome">
+  Nome do cliente: <input type="text" name="qualquercoisa"><br>
+  Outro campo: <input type="text" name="aquioutrocampo">
   <button>Olá mundo</button>
   </form>
   `);
 });
 
-app.post("/", (req, res) => {
-  res.send("Recebi o formulário");
+app.get("/testes/:idUsuarios?/:parametro?", (req, res) => {
+  // /profiles/3
+  // /profiles/?chave1=valor1&chave2=valor2&chave3=valor3
+  //
+  console.log(req.params);
+  console.log(req.query);
+  res.send(req.query.facebookprofile);
 });
 
-app.get("/contato", (req, res) => {
-  res.send("Obrigado por entrar em contato com a gente.");
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send(`O que você me enviou foi: ${req.body.qualquercoisa}`);
 });
 
 const port = 3000;
