ddbc349b782241c21093720d5f7bc73a457184b1
¡Duque <atanaelleonardo@gmail.com>
2022-10-17 00:35:47 -0300

    Modules (1)

--- --7.nodejs/aula01/app.js
+++ --7.nodejs/aula01/app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const axios = require("axios");
 const { Pessoa } = require("./mod1");
+const mod1 = require("./mod1");
 
 // axios("https://www.otaviomiranda.com.br/files/json/pessoas.json")
 //   .then((response) => console.table(response.data))
@@ -8,3 +9,4 @@ const { Pessoa } = require("./mod1");
 
 const p1 = new Pessoa("Luiz");
 console.table(p1);
+console.table(mod1);
