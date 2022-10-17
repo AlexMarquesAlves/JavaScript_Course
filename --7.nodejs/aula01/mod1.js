ce71565db6a1a70177405b181e6cb9061ebec80e
¡Duque <atanaelleonardo@gmail.com>
2022-10-16 23:57:40 -0300

    Exemplo 1 de module exports

---
+++ --7.nodejs/aula01/mod1.js
@@ -0,0 +1,12 @@
+const nome = "Luis";
+const sobrenome = "Miranda";
+
+const falaNome = () => {
+  console.log(nome, sobrenome);
+};
+
+// module.exports.nome = nome;
+// module.exports.sobrenome = sobrenome;
+// module.exports.falaNome = falaNome;
+
+console.log(module.exports);
