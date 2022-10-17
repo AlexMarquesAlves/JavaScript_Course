a63fdde498dfba1f088254458f1c980170a43603
¡Duque <atanaelleonardo@gmail.com>
2022-10-17 00:59:16 -0300

    Exemplo de Navegação entre pastas

--- --7.nodejs/aula02/app.js
+++ --7.nodejs/aula02/app.js
@@ -1,13 +1,18 @@
 /** Função */
-// const multiplicacao = require("./mod");
+// const multiplicacao = require("./B/C/D/mod");
 // console.log(multiplicacao(2, 2));
 
 /** Numero */
-// const n = require("./mod");
+// const n = require("./B/C/D/mod");
 // console.log(n);
 
 /** Classe */
-const Dog = require("./mod");
+const Dog = require("./z/mod2");
 
 const pug = new Dog("Rex");
 pug.latir();
+
+/** Navegação
+ * ./ => seguir adiante
+ * ../ => volver atrás
+ */
