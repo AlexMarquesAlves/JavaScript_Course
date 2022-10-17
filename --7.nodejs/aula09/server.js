e6968286a0bbcb40a04dae766cab7d197c419686
¡Duque <atanaelleonardo@gmail.com>
2022-10-17 18:18:07 -0300

    Express Router e Controllers

--- --7.nodejs/aula09/server.js
+++ --7.nodejs/aula09/server.js
@@ -1,32 +1,9 @@
 const express = require("express");
 const app = express();
+const routes = require("./routes");
 
-/**
- *         Criar  ||  Ler  || Atualizar || Apagar
- * CRUD => CREATE,||  READ,|| UPDATE,   || DELETE
- *         POST   ||  GET  || PUT       || DELETE
- */
-
-// http://meusite.com/ <- GET -> Entregue a página /
-// http://meusite.com/sobre <- GET -> Entregue a página /sobre
-// http://meusite.com/contato <- GET -> Entregue a página /contato
-
-app.get("/", (req, res) => {
-  res.send(`
-  <form action="/" method="POST">
-  Nome do cliente: <input type="text" name="nome">
-  <button>Olá mundo</button>
-  </form>
-  `);
-});
-
-app.post("/", (req, res) => {
-  res.send("Recebi o formulário");
-});
-
-app.get("/contato", (req, res) => {
-  res.send("Obrigado por entrar em contato com a gente.");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 const port = 3000;
 app.listen(port, () => {
