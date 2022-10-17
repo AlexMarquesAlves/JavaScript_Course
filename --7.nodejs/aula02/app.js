ec8468718f0761df0f1d0a1fa23952819197f2a3
¡Duque <atanaelleonardo@gmail.com>
2022-10-17 01:05:51 -0300

    Modules (2)

--- --7.nodejs/aula02/app.js
+++ --7.nodejs/aula02/app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 /** Função */
 // const multiplicacao = require("./B/C/D/mod");
 // console.log(multiplicacao(2, 2));
@@ -7,12 +9,15 @@
 // console.log(n);
 
 /** Classe */
-const Dog = require("./z/mod2");
+const Dog = require("./z/mod2.js");
 
 const pug = new Dog("Rex");
 pug.latir();
-
+console.log("");
 /** Navegação
  * ./ => seguir adiante
  * ../ => volver atrás
  */
+
+console.log(path.resolve(__dirname, "..", "..", "arquivos", "imagens"));
+console.log(path.resolve(__dirname, ".", ".", "arquivos", "imagens"));
