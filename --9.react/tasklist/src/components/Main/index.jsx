881ad794e0e2eb5ae9cea7802d9463d1814d13c4
¡Duque <atanaelleonardo@gmail.com>
2022-10-23 01:24:43 -0300

    Editando tarefas

--- --9.react/tasklist/src/components/Main/index.jsx
+++ --9.react/tasklist/src/components/Main/index.jsx
@@ -23,11 +23,12 @@ export default class Main extends Component {
       "Jantar",
       "Dormir",
     ],
+    index: -1,
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { tarefas } = this.state;
+    const { tarefas, index } = this.state;
     let { novaTarefa } = this.state;
     novaTarefa = novaTarefa.trim();
 
@@ -35,9 +36,20 @@ export default class Main extends Component {
 
     const novasTarefas = [...tarefas];
 
-    this.setState({
-      tarefas: [...novasTarefas, novaTarefa],
-    });
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: "",
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+        novaTarefa: "",
+      });
+    }
   };
 
   handleChange = (e) => {
@@ -46,7 +58,14 @@ export default class Main extends Component {
     });
   };
 
-  // handleEdit = (e, index) => {};
+  handleEdit = (e, index) => {
+    const { tarefas } = this.state;
+
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
+  };
 
   handleDelete = (e, index) => {
     const { tarefas } = this.state;
