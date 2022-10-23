caa872018b62465847b1ec3dc9d0167466e55b63
¡Duque <atanaelleonardo@gmail.com>
2022-10-22 23:06:37 -0300

    Estilizando tarefas

--- --9.react/tasklist/src/components/Main/index.jsx
+++ --9.react/tasklist/src/components/Main/index.jsx
@@ -5,9 +5,14 @@ import "./styles.scss";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { FaPlus } from "react-icons/fa";
 
+// tarefas
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { FaEdit, FaWindowClose } from "react-icons/fa";
+
 export default class Main extends Component {
   state = {
     novaTarefa: "",
+    tarefas: ["Preparar chá", "Beber chá", "Estudar"],
   };
 
   handleChange = (e) => {
@@ -18,7 +23,7 @@ export default class Main extends Component {
 
   render() {
     // eslint-disable-next-line no-unused-vars
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
@@ -30,6 +35,18 @@ export default class Main extends Component {
             <FaPlus />
           </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
