7c85079f4ac53d3cff5c22eb7f1f5804a7385278
¡Duque <atanaelleonardo@gmail.com>
2022-10-23 02:52:39 -0300

    Some codes updates

--- --8.api_rest/dist/app.js
+++ --8.api_rest/dist/app.js
@@ -2,30 +2,30 @@
 
 require('./database');
 
-var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 
-var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
-var _userRoutes = require('./routes/userRoutes'); var _userRoutes2 = _interopRequireDefault(_userRoutes);
-var _tokenRoutes = require('./routes/tokenRoutes'); var _tokenRoutes2 = _interopRequireDefault(_tokenRoutes);
 var _alunoRoutes = require('./routes/alunoRoutes'); var _alunoRoutes2 = _interopRequireDefault(_alunoRoutes);
 var _fotoRoutes = require('./routes/fotoRoutes'); var _fotoRoutes2 = _interopRequireDefault(_fotoRoutes);
+var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
+var _tokenRoutes = require('./routes/tokenRoutes'); var _tokenRoutes2 = _interopRequireDefault(_tokenRoutes);
+var _userRoutes = require('./routes/userRoutes'); var _userRoutes2 = _interopRequireDefault(_userRoutes);
 
 const whiteList = [
-  'https://react1.otaviomiranda.com.br',
-  'https://react2.otaviomiranda.com.br',
-  'http://localhost:3000',
+  "https://react1.otaviomiranda.com.br",
+  "https://react2.otaviomiranda.com.br",
+  "http://localhost:3003",
 ];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if(whiteList.indexOf(origin) !== -1 || !origin) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
-  }
+  },
 };
 
 class App {
@@ -40,15 +40,18 @@ class App {
     this.app.use(_helmet2.default.call(void 0, ));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
-    this.app.use('/images/', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
+    this.app.use(
+      "/images/",
+      _express2.default.static(_path.resolve.call(void 0, __dirname, "..", "uploads", "images"))
+    );
   }
 
   routes() {
-    this.app.use('/', _homeRoutes2.default);
-    this.app.use('/users/', _userRoutes2.default);
-    this.app.use('/tokens/', _tokenRoutes2.default);
-    this.app.use('/alunos/', _alunoRoutes2.default);
-    this.app.use('/fotos/', _fotoRoutes2.default);
+    this.app.use("/", _homeRoutes2.default);
+    this.app.use("/users/", _userRoutes2.default);
+    this.app.use("/tokens/", _tokenRoutes2.default);
+    this.app.use("/alunos/", _alunoRoutes2.default);
+    this.app.use("/fotos/", _fotoRoutes2.default);
   }
 }
 
--- --8.api_rest/src/app.js
+++ --8.api_rest/src/app.js
@@ -1,31 +1,31 @@
-import { resolve } from 'path';
+import { resolve } from "path";
 
-import './database';
+import "./database";
 
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
 
-import homeRoutes from './routes/homeRoutes';
-import userRoutes from './routes/userRoutes';
-import tokenRoutes from './routes/tokenRoutes';
-import alunoRoutes from './routes/alunoRoutes';
-import fotoRoutes from './routes/fotoRoutes';
+import alunoRoutes from "./routes/alunoRoutes";
+import fotoRoutes from "./routes/fotoRoutes";
+import homeRoutes from "./routes/homeRoutes";
+import tokenRoutes from "./routes/tokenRoutes";
+import userRoutes from "./routes/userRoutes";
 
 const whiteList = [
-  'https://react1.otaviomiranda.com.br',
-  'https://react2.otaviomiranda.com.br',
-  'http://localhost:3000',
+  "https://react1.otaviomiranda.com.br",
+  "https://react2.otaviomiranda.com.br",
+  "http://localhost:3003",
 ];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if(whiteList.indexOf(origin) !== -1 || !origin) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
-  }
+  },
 };
 
 class App {
@@ -40,15 +40,18 @@ class App {
     this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
-    this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
+    this.app.use(
+      "/images/",
+      express.static(resolve(__dirname, "..", "uploads", "images"))
+    );
   }
 
   routes() {
-    this.app.use('/', homeRoutes);
-    this.app.use('/users/', userRoutes);
-    this.app.use('/tokens/', tokenRoutes);
-    this.app.use('/alunos/', alunoRoutes);
-    this.app.use('/fotos/', fotoRoutes);
+    this.app.use("/", homeRoutes);
+    this.app.use("/users/", userRoutes);
+    this.app.use("/tokens/", tokenRoutes);
+    this.app.use("/alunos/", alunoRoutes);
+    this.app.use("/fotos/", fotoRoutes);
   }
 }
 
