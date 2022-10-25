545f951d39341d9472452c5d660e2cd45707f93f
¡Duque <atanaelleonardo@gmail.com>
2022-10-25 16:19:49 -0300

    Created store from redux

--- --9.react/react-base/src/App.jsx
+++ --9.react/react-base/src/App.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { Header } from "./components/Header";
 import { Routes } from "./routes";
 import { history } from "./services/history";
+import { store } from "./store";
 
 export const App = () => {
   return (
-    <Router history={history}>
-      <Header />
-      <Routes />
-      <global-styles />
-      <ToastContainer autoClose={3000} className={"toast-container"} />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Header />
+        <Routes />
+        <global-styles />
+        <ToastContainer autoClose={3000} className={"toast-container"} />
+      </Router>
+    </Provider>
   );
 };
