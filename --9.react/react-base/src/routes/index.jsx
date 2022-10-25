f42656bb58de7cfd12ce2946550ab538890bc3f0
¡Duque <atanaelleonardo@gmail.com>
2022-10-25 00:30:59 -0300

    🆕 Routes created

---
+++ --9.react/react-base/src/routes/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Login } from "../pages/Login";
+import { Page404 } from "../pages/Page404";
+
+export const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="*" component={Page404} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
