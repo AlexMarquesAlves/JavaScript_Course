f111bb049975b3fb98c1a77d7c368bf418fda937
¡Duque <atanaelleonardo@gmail.com>
2022-10-30 01:38:14 -0300

    🆕 Login Route created

--- --9.react/consumirapi/src/pages/Login/index.jsx
+++ --9.react/consumirapi/src/pages/Login/index.jsx
@@ -1,27 +1,9 @@
-import { useDispatch } from "react-redux";
-import * as exampleActions from "../../store/modules/example/actions";
 import { Container } from "../../styles/global-styles";
-import { Paragrafo, Title } from "./styles";
 
 export const Login = () => {
-  const dispatch = useDispatch();
-
-  function handleClick(e) {
-    e.preventDefault();
-
-    dispatch(exampleActions.clicaBotaoRequest());
-  }
-
   return (
     <Container>
-      <Title>
-        Login
-        <small>Oie</small>
-      </Title>
-      <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
-      <button type="button" onClick={handleClick}>
-        Enviar
-      </button>
+      <h1>Login</h1>
     </Container>
   );
 };
--- --9.react/consumirapi/src/pages/Login/styles.js
+++ --9.react/consumirapi/src/pages/Login/styles.js
@@ -2,18 +2,12 @@ import styled, { css } from "styled-components";
 
 export const Title = styled.h1`
   ${({ theme }) => css`
-    background: green;
 
-    small {
-      font-size: 12pt;
-      margin-left: 15px;
-      color: #999;
-    }
   `}
 `;
 
 export const Paragrafo = styled.p`
   ${({ theme }) => css`
-    font-size: 2rem;
+
   `}
 `;
