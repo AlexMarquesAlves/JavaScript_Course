b952da6f8f67b4c17e993be6fc7afcb85f106ea7
¡Duque <atanaelleonardo@gmail.com>
2022-10-30 03:49:04 -0300

    🆕 Form added

--- --9.react/consumirapi/src/pages/Register/index.jsx
+++ --9.react/consumirapi/src/pages/Register/index.jsx
@@ -1,9 +1,55 @@
+import React, { useState } from "react";
+// import { isEmail } from "validator";
 import { Container } from "../../styles/global-styles";
+import { Form } from "./styles";
 
 export const Register = () => {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let formErrors = false;
+  }
+
   return (
     <Container>
       <h1>Register</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <label htmlFor="nome">
+          Nome:
+          <input
+            type="text"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            placeholder="Seu nome"
+          />
+        </label>
+
+        <label htmlFor="email">
+          E-mail:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Seu e-mail"
+          />
+        </label>
+
+        <label htmlFor="password">
+          Senha:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Sua senha"
+          />
+        </label>
+
+        <button type="submit">Criar minha conta</button>
+      </Form>
     </Container>
   );
 };
