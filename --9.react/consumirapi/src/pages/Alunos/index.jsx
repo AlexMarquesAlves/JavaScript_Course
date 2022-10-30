605af58385f709bc5b31c93be7d0da83e1423252
¡Duque <atanaelleonardo@gmail.com>
2022-10-30 01:36:48 -0300

    🆕 Aluno Route created

---
+++ --9.react/consumirapi/src/pages/Alunos/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from "../../styles/global-styles";
+
+export const Alunos = () => {
+  return (
+    <Container>
+      <h1>Alunos</h1>
+    </Container>
+  );
+};
---
+++ --9.react/consumirapi/src/pages/Alunos/styles.js
@@ -0,0 +1,13 @@
+import styled, { css } from "styled-components";
+
+export const Title = styled.h1`
+  ${({ theme }) => css`
+
+  `}
+`;
+
+export const Paragrafo = styled.p`
+  ${({ theme }) => css`
+
+  `}
+`;
