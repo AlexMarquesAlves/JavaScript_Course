2d80491fded41a6d775e38f059790d6c5faf752e
¡Duque <atanaelleonardo@gmail.com>
2022-10-31 00:35:12 -0300

    🆕 Form structure created

--- --9.react/consumirapi/src/pages/Login/index.jsx
+++ --9.react/consumirapi/src/pages/Login/index.jsx
@@ -1,9 +1,27 @@
+import React from "react";
 import { Container } from "../../styles/global-styles";
+import { Form } from "./styles";
 
-export const Login = () => {
+export const Login = (props) => {
   return (
     <Container>
       <h1>Login</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Seu e-mail"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Sua senha"
+        />
+        <button type="submit">Acessar</button>
+      </Form>
     </Container>
   );
 };
