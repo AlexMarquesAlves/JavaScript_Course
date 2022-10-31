7ce1c97be8c34839cf8ce6110471484f83188c13
¡Duque <atanaelleonardo@gmail.com>
2022-10-31 00:47:06 -0300

    🆕 Componente loading

--- --9.react/consumirapi/src/pages/Alunos/index.jsx
+++ --9.react/consumirapi/src/pages/Alunos/index.jsx
@@ -2,17 +2,21 @@ import { get } from "lodash";
 import { useEffect, useState } from "react";
 import { FaEdit, FaUserCircle, FaWindowClose } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { Loading } from "../../components/Loading";
 import axios from "../../services/axios";
 import { Container } from "../../styles/global-styles";
 import { AlunoContainer, ProfilePicture } from "./styles";
 
 export const Alunos = () => {
   const [alunos, setAlunos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function getData() {
+      setIsLoading(true);
       const response = await axios.get("/alunos");
       setAlunos(response.data);
+      setIsLoading(false);
     }
 
     getData();
@@ -20,6 +24,8 @@ export const Alunos = () => {
 
   return (
     <Container>
+      <Loading isLoading={isLoading} />
+
       <h1>Alunos</h1>
 
       <AlunoContainer>
