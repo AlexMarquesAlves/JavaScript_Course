cda3cf9549f0b09131b20a32e8e9ac0e0cfb0da5
¡Duque <atanaelleonardo@gmail.com>
2022-11-06 12:17:49 -0300

    Seção 16 initial commit

--- 9.react/consumirapi/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { Aluno } from "./Aluno";
-export { Alunos } from "./Alunos";
-export { Fotos } from "./Fotos";
-export { Login } from "./Login";
-export { Page404 } from "./Page404";
-export { Register } from "./Register";
