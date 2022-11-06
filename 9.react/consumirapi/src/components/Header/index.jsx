f2172a35588726ebfbae6199e3d89d9202190be1
¡Duque <atanaelleonardo@gmail.com>
2022-11-06 00:09:23 -0300

    Logout

--- 9.react/consumirapi/src/components/Header/index.jsx
+++ 9.react/consumirapi/src/components/Header/index.jsx
@@ -1,20 +1,41 @@
 import React from "react";
-import { FaHome, FaSignInAlt, FaUserAlt } from "react-icons/fa";
+import { FaCircle, FaHome, FaPowerOff, FaSignInAlt, FaUserAlt } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { history } from "../../services/history";
+import * as actions from "../../store/modules/auth/actions";
 import { Nav } from "./styles";
 
 export const Header = () => {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(actions.loginFailure());
+    history.push("/");
+  };
+
   return (
     <Nav>
-      <Link to={"/"}>
+      <Link to="/">
         <FaHome size={24} />
       </Link>
-      <Link to={"/register"}>
+      <Link to="/register">
         <FaUserAlt size={24} />
       </Link>
-      <Link to={"/login"}>
-        <FaSignInAlt size={24} />
-      </Link>
+
+      {isLoggedIn ? (
+        <Link onClick={handleLogout} to="/logout">
+          <FaPowerOff size={24} />
+        </Link>
+      ) : (
+        <Link to="/login">
+          <FaSignInAlt size={24} />
+        </Link>
+      )}
+
+      {isLoggedIn && <FaCircle size={24} color="#66ff33" />}
     </Nav>
   );
 };
