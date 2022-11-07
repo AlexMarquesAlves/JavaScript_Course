c008482120e9f07afc0e64a0520a88e9a41d14e4
¡Duque <atanaelleonardo@gmail.com>
2022-11-06 23:33:16 -0300

    navigate e useNavigate: Redirecionando páginas

---
+++ 10.router-dom-v6/src/components/Redirect/index.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./style.scss";
+
+export type RedirectProps = {
+  title?: string;
+};
+
+export const Redirect = ({ title }: RedirectProps) => {
+  const [time, setTime] = useState(3);
+  const timeout = useRef(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    clearTimeout(timeout.current);
+
+    timeout.current = setTimeout(() => {
+      setTime((time) => time - 1);
+    }, 1000);
+
+    const timeEqualOrBellowZero = time <= 0;
+    if (timeEqualOrBellowZero) navigate("/");
+
+    return () => {
+      clearTimeout(timeout.current);
+    };
+  }, [time]);
+
+  return (
+    <div>
+      <h1>Get out of here in: {time}</h1>
+    </div>
+  );
+};
