f428286e09a74c09c95525445bd86fa5ed92c265
¡Duque <atanaelleonardo@gmail.com>
2022-11-08 00:48:03 -0300

    Começa com e Termina com

---
+++ 11.expressoes-regulares/aula07.js
@@ -0,0 +1,9 @@
+const { cpfs2 } = require("./base");
+
+// ^ -> Começa com
+// $ -> Termina com
+// [^] -> Negação
+
+const cpf = "254.224.877-45";
+const cpfRegExp = /^(\d{3}\.){2}\d{3}\-\d{2}$/gm;
+console.log(cpfs2.match(cpfRegExp));
