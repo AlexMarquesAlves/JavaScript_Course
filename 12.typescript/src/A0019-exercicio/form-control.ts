fdc675563a26d4f360ba50c66980e5b9df463a83
¡Duque <atanaelleonardo@gmail.com>
2022-11-10 19:06:27 -0300

    Exercício - Selecionando elementos da DOM

---
+++ 12.typescript/src/A0019-exercicio/form-control.ts
@@ -0,0 +1,7 @@
+const SHOW_ERROR_MESSAGES = "show-error-message";
+
+const form = document.querySelector(".form") as HTMLFormElement;
+const username = document.querySelector(".username") as HTMLInputElement;
+const email = document.querySelector(".email") as HTMLInputElement;
+const password = document.querySelector(".password") as HTMLInputElement;
+const password2 = document.querySelector(".password2") as HTMLInputElement;
