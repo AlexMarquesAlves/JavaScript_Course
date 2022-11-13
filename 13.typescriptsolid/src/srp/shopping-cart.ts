d6587d2358d4680e442ff2f683b7acc09be2d628
¡Duque <atanaelleonardo@gmail.com>
2022-11-13 18:31:23 -0300

    Getting items from class

--- 13.typescriptsolid/src/srp/shopping-cart.ts
+++ 13.typescriptsolid/src/srp/shopping-cart.ts
@@ -10,4 +10,15 @@ export class ShoppingCart {
   removeItem(index: number): void {
     this._items.splice(index, 1);
   }
+
+  get items(): Readonly<CartItem>[] {
+    return this._items;
+  }
 }
+
+const shoppingCart = new ShoppingCart();
+shoppingCart.addItem({ name: "Camisa", price: 49.9 });
+shoppingCart.addItem({ name: "Caderno", price: 9.9 });
+shoppingCart.addItem({ name: "Lápis", price: 1.59 });
+
+console.log(shoppingCart.items);
