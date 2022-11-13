c36a594fdba731b4d2b3ac31a017c759f1d55b1e
¡Duque <atanaelleonardo@gmail.com>
2022-11-13 18:37:55 -0300

    𝔾𝕖𝕥𝕥𝕚𝕟𝕘 𝕥𝕠𝕥𝕒𝕝 𝕧𝕒𝕝𝕦𝕖

--- 13.typescriptsolid/src/srp/shopping-cart.ts
+++ 13.typescriptsolid/src/srp/shopping-cart.ts
@@ -14,11 +14,18 @@ export class ShoppingCart {
   get items(): Readonly<CartItem>[] {
     return this._items;
   }
+
+  total(): number {
+    return +this._items
+      .reduce((total, next) => total + next.price, 0)
+      .toFixed(2);
+  }
 }
 
 const shoppingCart = new ShoppingCart();
-shoppingCart.addItem({ name: "Camisa", price: 49.9 });
-shoppingCart.addItem({ name: "Caderno", price: 9.9 });
+shoppingCart.addItem({ name: "Camisa", price: 49.91 });
+shoppingCart.addItem({ name: "Caderno", price: 9.9123 });
 shoppingCart.addItem({ name: "Lápis", price: 1.59 });
 
 console.log(shoppingCart.items);
+console.log(shoppingCart.total());
