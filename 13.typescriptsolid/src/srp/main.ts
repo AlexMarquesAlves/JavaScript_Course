7db061b2b5952dc305369b07a39c862677865029
¡Duque <atanaelleonardo@gmail.com>
2022-11-13 19:36:16 -0300

    Merge branch 'TS-S.O.L.I.D' of https://github.com/AlexMarquesAlves/JavaScript_Course into TS-S.O.L.I.D

--- 13.typescriptsolid/src/srp/main.ts
+++ 13.typescriptsolid/src/srp/main.ts
@@ -1,5 +1,4 @@
 import { Order } from "./entities/order";
-import { Product } from "./entities/product";
 import { ShoppingCart } from "./entities/shopping-cart";
 import { Messaging } from "./services/messaging";
 import { Persistency } from "./services/persistency";
@@ -8,10 +7,9 @@ const shoppingCart = new ShoppingCart();
 const messaging = new Messaging();
 const persistency = new Persistency();
 const order = new Order(shoppingCart, messaging, persistency);
-
-shoppingCart.addItem(new Product("Camisa", 49.91));
-shoppingCart.addItem(new Product("Caderno", 9.9123));
-shoppingCart.addItem(new Product("Lápis", 1.59));
+shoppingCart.addItem({ name: "Camisa", price: 49.91 });
+shoppingCart.addItem({ name: "Caderno", price: 9.9123 });
+shoppingCart.addItem({ name: "Lápis", price: 1.59 });
 
 console.log(shoppingCart.items);
 console.log(shoppingCart.total());
