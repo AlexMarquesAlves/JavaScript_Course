886a430d934d36e891a3a3e25413210e5c538bbe
¡Duque <atanaelleonardo@gmail.com>
2022-11-13 19:23:27 -0300

    𝔻𝕖𝕡𝕖𝕟𝕕𝕖𝕟𝕔𝕪 𝕚𝕟𝕛𝕖𝕔𝕥𝕚𝕠𝕟 𝕠𝕟 𝕆𝕣𝕕𝕖𝕣 𝕔𝕝𝕒𝕤𝕤

--- 13.typescriptsolid/src/srp/main.ts
+++ 13.typescriptsolid/src/srp/main.ts
@@ -1,8 +1,12 @@
+import { Messaging } from "./messaging";
 import { Order } from "./order";
+import { Persistency } from "./persistency";
 import { ShoppingCart } from "./shopping-cart";
 
 const shoppingCart = new ShoppingCart();
-const order = new Order(shoppingCart);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
 shoppingCart.addItem({ name: "Camisa", price: 49.91 });
 shoppingCart.addItem({ name: "Caderno", price: 9.9123 });
 shoppingCart.addItem({ name: "Lápis", price: 1.59 });
