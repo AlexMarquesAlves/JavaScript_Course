2c68a30361461693eb4602217eff37c2ff5ca868
¡Duque <atanaelleonardo@gmail.com>
2022-11-13 19:29:28 -0300

    𝕊ℝℙ - 𝕊𝕚𝕟𝕘𝕝𝕖 ℝ𝕖𝕤𝕡𝕠𝕟𝕤𝕚𝕓𝕚𝕝𝕚𝕥𝕪 ℙ𝕣𝕚𝕟𝕔𝕚𝕡𝕝𝕖

--- 13.typescriptsolid/src/srp/main.ts
+++ 13.typescriptsolid/src/srp/main.ts
@@ -1,15 +1,17 @@
 import { Messaging } from "./messaging";
 import { Order } from "./order";
 import { Persistency } from "./persistency";
+import { Product } from "./product";
 import { ShoppingCart } from "./shopping-cart";
 
 const shoppingCart = new ShoppingCart();
 const messaging = new Messaging();
 const persistency = new Persistency();
 const order = new Order(shoppingCart, messaging, persistency);
-shoppingCart.addItem({ name: "Camisa", price: 49.91 });
-shoppingCart.addItem({ name: "Caderno", price: 9.9123 });
-shoppingCart.addItem({ name: "Lápis", price: 1.59 });
+
+shoppingCart.addItem(new Product("Camisa", 49.91));
+shoppingCart.addItem(new Product("Caderno", 9.9123));
+shoppingCart.addItem(new Product("Lápis", 1.59));
 
 console.log(shoppingCart.items);
 console.log(shoppingCart.total());
